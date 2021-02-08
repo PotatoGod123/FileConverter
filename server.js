a17d3ac8e904128a8ef8bbf3195f75d76011b047
Cristian Robles <formymoment12@gmail.com>
2021-02-08 15:24:10 -0600

    Holding stages for other computer, this has conversion api and form code logic

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const express = require('express');
 const pg = require('pg');
 const superagent = require('superagent');
 const cors = require('cors');
-const formidable = require('formidable');;
+const formidable = require('formidable');
 const base64 = require('file-base64');
 const { ConvertAPI } = require('convertapi');
 const convertapi = new ConvertAPI(process.env.SECRET, { conversionTimeout: 60 });
+const fs = require('fs');
+const path = require('path');
 
 
 
@@ -43,28 +45,38 @@ function aboutUsHandler(request, response) {
 }
 
 function formHandler(request, response) {
-  response.status(200).render('pages/form');
+  const extensions = ['png','jpg','pdf','docx','xlsx','html','gif','jpeg','zip','doc','webp','txt'];
+  const conversionTypes = ['jpg','png','pdf','pdfa','tiff','watermark','webp','gif','doc','txt','jpeg','docx','html','jpg','mhtml','odt','pdf','pdfa','png','rtx','tiff','txt','webp','xps','zip','compress','decompress','decrypt','encrypt','extract','jpg','pdfa','png','pptx','repair','txt','zip','watermark'];
+  extensions.sort();
+  conversionTypes.sort();
+  let uniqueTypes= new Set(conversionTypes);
+  response.status(200).render('pages/form',{Files:extensions,FilesTwo:uniqueTypes});
 }
 
 function saveHandler(request, response) {
   let form = new formidable.IncomingForm();
 
   form.parse(request, (err, fields, files) => {
-    // console.log(fields);
-    // console.log(files);
-    // console.log(files.fileUpload.name);
+    console.log(fields);
+    console.log(files);
+    console.log(files.fileUpload.name+' -------------- ');
 
     let holder = files.fileUpload.path;
-    let newPath= __dirname+holder;
+    let newPath= path.join(__dirname,'uploads')+'/'+files.fileUpload.name;
+    let fileData = fs.readFileSync(holder);
     console.log(newPath);
     // base64.encode(holder, (err, base64String) => {
 
     // });
-
-    // convertapi.convert(fields.fileConversion,{File:holder})
-    //   .then(result=>{
-    //     console.log(result);
-    //   }).catch(err=>console.error(err));
+    fs.writeFile(newPath, fileData, function(err){
+      return err? console.log(err): console.log('Nice uploaded!');
+    });
+    
+    convertapi.convert(fields.fileConversion,{File:newPath})
+      .then(result=>{
+        console.log(result.response.Files[0]);
+        // console.log(result.file);
+      }).catch(err=>console.error(err));
  
     // superagent.get(url)
     //   .set('Content-Type', `application/json`)
